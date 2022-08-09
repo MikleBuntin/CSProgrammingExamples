f62db97bb51a882cbc10616f16251133bc967d08
MikleBuntin <BuntinMA@mail.ru>
2022-08-09 15:16:28 +0300

    Summ of random integers

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,5 +1,8 @@
-﻿double NumberA = 12;
-double NumberB = 5;
-double div = NumberA / NumberB;
+﻿int NumberA = new Random().Next(1, 10);
+int NumberB = new Random().Next(1, 10);
+double div = NumberA + NumberB;
+
+Console.WriteLine(NumberA);
+Console.WriteLine(NumberB);
 Console.WriteLine(div);
  
