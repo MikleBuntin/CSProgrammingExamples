2539d7e063fbcd55ec2f6ef9f895ca7ef33edf8a
MikleBuntin <BuntinMA@mail.ru>
2022-08-09 15:43:56 +0300

    Настройка проограммы приветствия

--- Example005_IfElse/Program.cs
+++ Example005_IfElse/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Введите имя пользователя: ");
+string username = console.readline();
+if (username == "Люля");
+    {
+    Console.writeline("Здравствуй, солнышко!!!");
+    }
+else
+    {
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+    }
